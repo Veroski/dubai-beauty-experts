fb6c45c961a669d493cc829dac14a34590e94d8d
Veroski <guillemvera@gmail.com>
2026-02-07 22:31:30 +0100

    cambios

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import DubaiParallax from "../components/DubaiParallax";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.hero} aria-label="Hero">
-      <div className={styles.content}>
-        <DubaiParallax className={styles.dubai} />
-      </div>
-    </main>
+
   );
 }
 
